2dac4534d5a05461dc2d859790f31a6f9a6f8064
PyTransit · Rails, Routes & Runways <abdounour.misbah@yahoo.com>
2025-11-15 23:01:40 +0100

    Add the function clock()
    clock() est chargé de cadencer les évènements
    c'est aussi lui qui lance et arrête la simulation

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 from random import uniform
 import numpy as np
+import time
 
 # === MODEL FUNCTIONS ===
 
@@ -38,3 +39,35 @@ def bus_departure(buspax,seats,stand_capacity):
       no_more_seats = True
 
   return (passengers,no_more_seats)
+
+# === EVENT MANAGEMENT ===
+
+def clock(period,freq_train,freq_bus):
+   # cadence les opérations
+
+   # initialisations
+   tps = 1 # compteur de minutes
+   pax_arret = 0 # nombre de personnes attendant le bus
+   trains = {} # trains arrivés et passagers en transit vers le bus
+               # clés = heure d'arrivée du train
+               # valeur = nombre de passagers cherchant le bus   
+
+   while tps<=period:
+      if tps%freq_train==0:
+          trains[tps] = goto_bus(train_arrival())
+
+      for arrival,transit_pax in trains.copy().items():
+          j = arrive_at_bus_stop(transit_pax,arrival,tps)
+          if j==0: # nettoyer le dictionnaire en enlevant les trop bas
+             del trains[arrival]
+          else:
+             pax_arret += j
+
+      if tps%freq_bus==0:
+          gone, no_more_seats = bus_departure(pax_arret,22,64)
+          pax_arret -= gone
+  
+      time.sleep(1) # WARNING : Si period est très élevé, cette instruction peut causer des problèmes de latence sur la machine !
+      tps+=1
+
+   print("Simulation terminée")
